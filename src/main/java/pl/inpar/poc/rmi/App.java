707a6ceb7b19515a2ba5f53e51a5156a67f83425
Dariusz Raćkowski <drackows@gmail.com>
2014-02-02 20:49:05 +0100

    podstawa

---
+++ src/main/java/pl/inpar/poc/rmi/App.java
@@ -0,0 +1,13 @@
+package pl.inpar.poc.rmi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
